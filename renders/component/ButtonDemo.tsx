e26f3c3d679953951ff0f4785aa910e9bc444538
m4dd0c <manishsuthar078@gmail.com>
2026-01-24 19:10:09 +0530

    feat: demo file added

---
+++ renders/component/ButtonDemo.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/registry/new-york/ui/button";
+import React from "react";
+
+const ButtonDemo = () => {
+  return <Button variant={"default"}>Submit</Button>;
+};
+
+export default ButtonDemo;
